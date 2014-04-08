7321243aad12b1d50038eb4bf18362a56de0db89
Kyle Conkright <kyleconkright@gmail.com>
2014-04-08 10:00:13 -0700

    Album Data File Added

--- lib/script.coffee
+++ lib/script.coffee
@@ -12,9 +12,15 @@ $ ->
 				.append('<li><p class="date">' + show[1] + '-' + show[2] + '-' + show[0] + '</p><p>' + this.venue.city + ', ' + this.venue.region + ' <span>at</span> ' + this.venue.name + '</p><p class="ticket">Tickets <i class="fa fa-ticket"></i></li>')
 				.appendTo('.shows')
 
-	# $.ajax
-	# 	url: 'data/data.json'
-	# 	type: 'GET'
-	# 	dataType: 'json'
-	# 	success: (results) ->
-			
+	$.ajax
+		url: 'data/data.json'
+		type: 'GET'
+		dataType: 'json'
+		success: (results) ->
+			$('<div class="releases"></div>').appendTo('.music')
+			$.each results.releases, ->	
+				$('<div class="release"></div>')
+				.append('<h3>' + this.title + '</h3><img src="' + this.art + '"><p><a href="#">Listen</a></p><p>Buy: <a href="#">MP3</a> / <a href="#">CD</a></p>')	
+				.appendTo('.music .releases')
+
+
--- tmp/script.js
+++ tmp/script.js
@@ -1,6 +1,6 @@
 (function() {
   $(function() {
-    return $.ajax({
+    $.ajax({
       url: 'http://api.bandsintown.com/artists/joepurdy/events.json?callback=?&app_id=joepurdy',
       type: 'GET',
       dataType: 'jsonp',
@@ -10,6 +10,17 @@
           date = this.datetime.split("T");
           show = date[0].split("-");
           return $('<ul></ul>').append('<li><p class="date">' + show[1] + '-' + show[2] + '-' + show[0] + '</p><p>' + this.venue.city + ', ' + this.venue.region + ' <span>at</span> ' + this.venue.name + '</p><p class="ticket">Tickets <i class="fa fa-ticket"></i></li>').appendTo('.shows');
+        });
+      }
+    });
+    return $.ajax({
+      url: 'data/data.json',
+      type: 'GET',
+      dataType: 'json',
+      success: function(results) {
+        $('<div class="releases"></div>').appendTo('.music');
+        return $.each(results.releases, function() {
+          return $('<div class="release"></div>').append('<h3>' + this.title + '</h3><img src="' + this.art + '"><p><a href="#">Listen</a></p><p>Buy: <a href="#">MP3</a> / <a href="#">CD</a></p>').appendTo('.music .releases');
         });
       }
     });
